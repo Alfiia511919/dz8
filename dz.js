0a014b58f248c8da4a8e7ba6c5ed44f2bd931d78
Alfiia511919 <puma432@mail.ru>
2022-02-02 20:59:36 +0600

    first commit

---
+++ dz.js
@@ -0,0 +1,27 @@
+class Transport {
+constructor(color, speed, titel){
+    this.color = color;
+    this.speed = speed;
+    this.titel = titel;
+}
+     startEngine(){
+        console.log( "is started");
+}
+}
+
+class Car extends Transport{
+       
+    }
+startEngine (`${this.titel}"takeoff"`) 
+    console.log("takeoff");
+
+const airplane = new Transport(
+   
+)
+Transport.startEngine("  is started!");
+
+const helicopter = new Transport(
+   
+)
+helicopter.startEngine("is started!");
+
